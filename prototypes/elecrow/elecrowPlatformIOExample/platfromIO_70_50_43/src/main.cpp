5e8239479deb4208cba35243f23ffa11caa6c140
John Savoy <jsavoy@gmail.com>
2026-07-05 12:37:19 -0500

    new dashOne prototype

---
+++ prototypes/elecrow/elecrowPlatformIOExample/platfromIO_70_50_43/src/main.cpp
@@ -0,0 +1,150 @@
+#include "pins_config.h"
+#include "LovyanGFX_Driver.h"
+
+#include <Arduino.h>
+#include <lvgl.h>
+#include <Wire.h>
+#include <SPI.h>
+
+#include <stdbool.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+#include "ui.h"
+
+LGFX gfx;
+
+/* Change to your screen resolution */
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t *buf;
+static lv_color_t *buf1;
+
+uint16_t touch_x, touch_y;
+
+//  Display refresh
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
+  if (gfx.getStartCount() > 0) {
+    gfx.endWrite();
+  }
+  gfx.pushImageDMA(area->x1, area->y1, area->x2 - area->x1 + 1, area->y2 - area->y1 + 1, (lgfx::rgb565_t *)&color_p->full);
+
+  lv_disp_flush_ready(disp);  //	Tell lvgl that the refresh is complete
+}
+
+//  Read touch
+void my_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data )
+{
+  data->state = LV_INDEV_STATE_REL;// The state of data existence when releasing the finger
+  bool touched = gfx.getTouch( &touch_x, &touch_y );
+  if (touched)
+  {
+    data->state = LV_INDEV_STATE_PR;
+
+    //  Set coordinates
+    data->point.x = touch_x;
+    data->point.y = touch_y;
+  }
+}
+
+bool i2cScanForAddress(uint8_t address) {
+  Wire.beginTransmission(address);
+  return (Wire.endTransmission() == 0);
+}
+
+// Wrapper function for sending I2C commands
+void sendI2CCommand(uint8_t command) {
+  uint8_t error;
+  // Start sending commands to the specified address
+  Wire.beginTransmission(0x30);
+  // Send command
+  Wire.write(command);
+  //  End transmission and return status
+  error = Wire.endTransmission();
+
+  if (error == 0) {
+    Serial.print("command 0x");
+    Serial.print(command, HEX);
+    Serial.println(" Sent successfully");
+  } else {
+    Serial.print("Command sent error, error code:");
+    Serial.println(error);
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200); 
+
+  pinMode(19, OUTPUT);
+
+  Wire.begin(15, 16);
+  delay(50);
+  while (1) {
+    if (i2cScanForAddress(0x30) && i2cScanForAddress(0x5D)) {
+      Serial.print("The microcontroller is detected: address 0x");
+      Serial.println(0x30, HEX);
+      Serial.print("The microcontroller is detected: address 0x");
+      Serial.println(0x5D, HEX);
+      break;
+    } else {
+      Serial.print("No microcontroller was detected: address 0x");
+      Serial.println(0x30, HEX);
+      Serial.print("No microcontroller was detected: address 0x");
+      Serial.println(0x5D, HEX);
+      //Prevent the microcontroller did not start to adjust the bright screen
+      sendI2CCommand(0x19);    // 0x19 : Activate touch screen
+      pinMode(1, OUTPUT);
+      digitalWrite(1, LOW);
+      
+      delay(120);
+      pinMode(1, INPUT);
+
+      delay(100);
+    }
+  }
+  // Start sending command 0x10 to address 0x30
+  sendI2CCommand(0x10);  // 0x10 is the brightest backlight.   
+
+  // Init Display
+  gfx.init();
+  gfx.initDMA();
+  gfx.startWrite();
+  gfx.fillScreen(TFT_BLACK);
+
+  lv_init();
+  size_t buffer_size = sizeof(lv_color_t) * LCD_H_RES * LCD_V_RES;
+  buf = (lv_color_t *)heap_caps_malloc(buffer_size, MALLOC_CAP_SPIRAM);
+  buf1 = (lv_color_t *)heap_caps_malloc(buffer_size, MALLOC_CAP_SPIRAM);
+
+  lv_disp_draw_buf_init(&draw_buf, buf, buf1, LCD_H_RES * LCD_V_RES);
+
+  // Initialize display
+  static lv_disp_drv_t disp_drv;
+  lv_disp_drv_init(&disp_drv);
+  // Change the following lines to your display resolution
+  disp_drv.hor_res = LCD_H_RES;
+  disp_drv.ver_res = LCD_V_RES;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register(&disp_drv);
+
+  // Initialize input device driver program
+  static lv_indev_drv_t indev_drv;
+  lv_indev_drv_init(&indev_drv);
+  indev_drv.type = LV_INDEV_TYPE_POINTER;
+  indev_drv.read_cb = my_touchpad_read;
+  lv_indev_drv_register(&indev_drv);
+
+  delay(100);
+  gfx.fillScreen(TFT_BLACK);
+  // lv_demo_widgets();// Main UI interface
+  ui_init();
+
+  Serial.println( "Setup done" );
+}
+
+void loop()
+{
+  lv_timer_handler(); /* let the GUI do its work */
+  delay(1);
+}
